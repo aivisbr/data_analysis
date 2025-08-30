9a52cc4fec9c712a944ebc93d2d809fa18f7b86b
AB <32178546+aivisbr@users.noreply.github.com>
2025-08-30 17:39:19 +0300

    Create main.py

---
+++ 018_Realtime_API_SIP/main.py
@@ -0,0 +1,83 @@
+# https://platform.openai.com/docs/guides/realtime-sip#overview
+
+from dotenv import load_dotenv # pip install python-dotenv
+from flask import Flask, request, Response, jsonify, make_response
+from openai import OpenAI, InvalidWebhookSignatureError
+import asyncio
+import json
+import os
+import requests
+import time
+import threading
+import websockets
+
+# Load the .env file
+load_dotenv()
+
+app = Flask(__name__)
+client = OpenAI(
+    webhook_secret=os.environ["OPENAI_WEBHOOK_SECRET"]
+)
+
+AUTH_HEADER = {
+    "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY")
+}
+
+call_accept = {
+    "type": "realtime",
+    "instructions": "You are a support agent.",
+    "model": "gpt-realtime", # gpt-4o-realtime-preview-2024-12-17
+}
+
+response_create = {
+    "type": "response.create",
+    "response": {
+        "instructions": (
+            "Say to the user 'Thank you for calling, how can I help you'"
+        )
+    },
+}
+
+
+async def websocket_task(call_id):
+    try:
+        async with websockets.connect(
+            "wss://api.openai.com/v1/realtime?call_id=" + call_id,
+            additional_headers=AUTH_HEADER,
+        ) as websocket:
+            await websocket.send(json.dumps(response_create))
+
+            while True:
+                response = await websocket.recv()
+                print(f"Received from WebSocket: {response}")
+    except Exception as e:
+        print(f"WebSocket error: {e}")
+
+
+@app.route("/", methods=["POST"])
+def webhook():
+    try:
+        event = client.webhooks.unwrap(request.data, request.headers)
+
+        if event.type == "realtime.call.incoming":
+            requests.post(
+                "https://api.openai.com/v1/realtime/calls/"
+                + event.data.call_id
+                + "/accept",
+                headers={**AUTH_HEADER, "Content-Type": "application/json"},
+                json=call_accept,
+            )
+            threading.Thread(
+                target=lambda: asyncio.run(
+                    websocket_task(event.data.call_id)
+                ),
+                daemon=True,
+            ).start()
+            return Response(status=200)
+    except InvalidWebhookSignatureError as e:
+        print("Invalid signature", e)
+        return Response("Invalid signature", status=400)
+
+
+if __name__ == "__main__":
+    app.run(port=8000)
